154cf1402dfdaf6583ddf36460fa9a9763273706
sfm444 <s_schris@hotmail.com>
2018-10-20 12:53:33 +1100

    Completed section 2 of Udemy Tutorial

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-    return <input /> // React.createElement on compile to ES5
-};
+class SearchBar extends Component {
+    
+    constructor(props) {
+        super(props);
+
+        this.state = { term: ''};
+    }
+
+    render() {
+        return (
+            <div className="search-bar">
+            <input 
+                value= {this.state.term}
+                onChange = {event => this.onInputChange(event.target.value)}/>
+           </div>
+        );
+    }
+
+    onInputChange(term) {
+        this.setState({term});
+        this.props.onSearchTermChange(term);
+
+    }
+}
 
 export default SearchBar;
---
+++ src/components/video_list_item.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const VideoListItem = ({video, onVideoSelect}) => {
+   // const video = props.video; Superceded by {video} property in function
+   const imageUrl = video.snippet.thumbnails.default.url;
+
+    return (
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
+        <div className="video-list media">
+            <div className="media-left">
+                <img className="media-object" src={imageUrl} ></img>    
+            </div>
+            
+            <div className="media-body">
+                <div className="media-heading">{video.snippet.title}
+                </div>
+            </div>
+        </div>
+
+    </li>
+    );
+};
+
+export default VideoListItem;
